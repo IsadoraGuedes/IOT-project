393bf20a10e65f19523fcdf60337132e9752857b
Isadora Guedes <isadora.guedes@sap.com>
2024-12-05 12:59:06 -0300

    Adiciona table inicial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,109 +2,126 @@ import { useState } from "react";
 import mqtt from "mqtt";
 import { PatientForm } from "./patientForm";
 import prisma from "./database/db";
+import MetricsTable from "./metricsTable";
+import Typography from "@mui/material/Typography";
 
 export async function getServerSideProps() {
   console.log("getServerSideProps executing...");
 
-  const patientData = await prisma.patientData.findMany();
+  const patientData = await prisma.patientData.findMany({
+    where: {
+      name: "Isadora",
+    },
+  });
 
   // Convert Date objects to ISO strings for serialization
-  const serializedRecipes = patientData.map(patientData => ({
-      ...patientData,
-      createdAt: patientData.createdAt.toISOString(),
-      // updatedAt: recipe.updatedAt.toISOString()
+  const serializedRecipes = patientData.map((patientData) => ({
+    ...patientData,
+    createdAt: patientData.createdAt.toISOString(),
+    // updatedAt: recipe.updatedAt.toISOString()
   }));
 
   return {
-      props: { patientData: serializedRecipes },
+    props: { patientData: serializedRecipes },
   };
 }
 
 export default function Home({ patientData }) {
   const [messages, setMessages] = useState<string[]>([]);
 
-    // Connect and subscribe directly
-    const brokerUrl = 'wss://broker.hivemq.com:8884/mqtt';
-    const topic = 'topic_sensor_desconforto_nicisa';
+  // Connect and subscribe directly
+  const brokerUrl = "wss://broker.hivemq.com:8884/mqtt";
+  const topic = "topic_sensor_desconforto_nicisa";
 
-    const client = mqtt.connect(brokerUrl);
+  const client = mqtt.connect(brokerUrl);
 
-    client.on('connect', () => {
-        console.log('Connected to MQTT broker');
-        client.subscribe(topic, (err) => {
-            if (err) {
-                console.error('Failed to subscribe:', err.message);
-            } else {
-                console.log(`Subscribed to topic: ${topic}`);
-            }
-        });
+  client.on("connect", () => {
+    console.log("Connected to MQTT broker");
+    client.subscribe(topic, (err) => {
+      if (err) {
+        console.error("Failed to subscribe:", err.message);
+      } else {
+        console.log(`Subscribed to topic: ${topic}`);
+      }
     });
+  });
 
-    client.on('message', async (topic, message) => {
-        const msg = message.toString();
-        console.log(`Message received on ${topic}: ${msg}`);
-        setMessages((prevMessages) => [...prevMessages, msg]);
+  client.on("message", async (topic, message) => {
+    const msg = message.toString();
+    console.log(`Message received on ${topic}: ${msg}`);
+    setMessages((prevMessages) => [...prevMessages, msg]);
 
-        console.log(JSON.stringify({
+    console.log(
+      JSON.stringify({
+        name: formData.nomeDoPaciente,
+        session: formData.numeroDaSessao,
+        value: parseFloat(msg),
+        bodyArea: formData.regiaoDeMedicao,
+        createdAt: new Date().toISOString(),
+      })
+    );
+    try {
+      await fetch("http://localhost:3000/api/patient-data", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
           name: formData.nomeDoPaciente,
           session: formData.numeroDaSessao,
           value: parseFloat(msg),
           bodyArea: formData.regiaoDeMedicao,
           createdAt: new Date().toISOString(),
-      }))
-        try {
-          await fetch('http://localhost:3000/api/patient-data', {
-              method: 'POST',
-              headers: {
-                  'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({
-                  name: formData.nomeDoPaciente,
-                  session: formData.numeroDaSessao,
-                  value: parseFloat(msg),
-                  bodyArea: formData.regiaoDeMedicao,
-                  createdAt: new Date().toISOString(),
-              }),
-          });
-          console.log('Data added to PatientData');
-      } catch (error) {
-          console.error('Failed to add data:', error);
-      }
-    });
+        }),
+      });
+      console.log("Data added to PatientData");
+    } catch (error) {
+      console.error("Failed to add data:", error);
+    }
+  });
 
-    client.on('error', (err) => {
-        console.error('MQTT connection error:', err.message);
-    });
+  client.on("error", (err) => {
+    console.error("MQTT connection error:", err.message);
+  });
 
-    const [formData, setFormData] = useState({
-      nomeDoPaciente: '',
-        regiaoDeMedicao: '',
-        numeroDaSessao: ''
-    });
+  const [formData, setFormData] = useState({
+    nomeDoPaciente: "",
+    regiaoDeMedicao: "",
+    numeroDaSessao: "",
+  });
 
-    const handleFormSubmit = (data) => {
-        setFormData(data); // Set the form data to display other components
-    };
+  const handleFormSubmit = (data) => {
+    setFormData(data); // Set the form data to display other components
+  };
 
   return (
-    <div style={{ padding: '20px' }}>
-      <h1>MQTT Data Interface</h1>
+    <div style={{ padding: "20px" }}>
       <ul>
         {messages.map((msg: any, index: any) => (
           <li key={index}>{msg}</li>
         ))}
       </ul>
       <div>
-      <h1>Bem-vindo à Tela Inicial</h1>
-            {!formData ? (
-                <PatientForm onSubmit={handleFormSubmit} />
-            ) : (
-                <div>
-                     <pre>{JSON.stringify(patientData, null, 2)}</pre>
-                </div>
-            )}
+        {formData?.nomeDoPaciente === "" ? (
+          <PatientForm onSubmit={handleFormSubmit} />
+        ) : (
+          <div>
+            <Typography variant="h3" gutterBottom>
+              Dados das sessões
+            </Typography>
+            <Typography variant="h6" gutterBottom>
+              Paciente: { formData.nomeDoPaciente }
+            </Typography>
+            <Typography variant="h6" gutterBottom>
+              Região: { formData.regiaoDeMedicao }
+            </Typography>
+            <Typography variant="h6" gutterBottom>
+              Sessão atual: { formData.numeroDaSessao }
+            </Typography>
+            <MetricsTable data={patientData} />
+          </div>
+        )}
       </div>
     </div>
   );
 }
-
---
+++ src/pages/metricsTable.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+export default function MetricsTable({ data }) {
+    console.log('data: ', data)
+    return (
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="patient data">
+          <TableHead>
+            <TableRow>
+              <TableCell>Sessão</TableCell>
+              <TableCell align="right">Valor medido</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.session}
+                </TableCell>
+                <TableCell align="right">{row.value}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+}
